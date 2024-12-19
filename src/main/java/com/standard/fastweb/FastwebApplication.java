b2ff8fb249a0befe37d60224b3c551349b9bbfea
donghee.shin <tlsehdgml159@gmail.com>
2024-12-19 14:25:41 +0900

    Initial commit

---
+++ src/main/java/com/standard/fastweb/FastwebApplication.java
@@ -0,0 +1,13 @@
+package com.standard.fastweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastwebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastwebApplication.class, args);
+    }
+
+}
